2c4a77f83ed19095e5a6f2a9e4caf99fac2a2a5c
Tsuzu <8574909+tsuzu@users.noreply.github.com>
2024-04-29 14:56:15 +0900

    Remove python code

--- app/hid.py
+++
@@ -1,7 +0,0 @@
-def send(hid_path, control_keys, hid_keycode):
-    with open(hid_path, 'wb+') as hid_handle:
-        buf = [0] * 8
-        buf[0] = control_keys
-        buf[2] = hid_keycode
-        hid_handle.write(bytearray(buf))
-        hid_handle.write(bytearray([0] * 8))
